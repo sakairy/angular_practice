6c1d79162dfa49ca8294cb6be7e9e98eb7bd0f1e
sr <sakai.ptw@gmail.com>
2024-05-19 14:49:28 +0900

    new component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HelloComponent } from './content/hello/hello.component';
 import { GraphMenuComponent } from './content/graph-menu/graph-menu.component';
+import { IonicComponent } from './content/ionic/ionic.component';
 
 export const routes: Routes = [
 	{
@@ -11,7 +12,10 @@ export const routes: Routes = [
 		path:"graph-menu",
 		component:GraphMenuComponent,
 	},
-
+	{
+		path:"ionic",
+		component:IonicComponent,
+	},
 
 
 
---
+++ src/app/content/ionic/ionic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ionic',
+  standalone: true,
+  imports: [],
+  templateUrl: './ionic.component.html',
+  styleUrl: './ionic.component.scss'
+})
+export class IonicComponent {
+
+}
