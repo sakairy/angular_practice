6458e14cbca95e9fd5119d2c3bcce98ae488ba98
sr <sakai.ptw@gmail.com>
2024-05-26 02:06:18 +0900

    add notification component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HelloComponent } from './content/hello/hello.component';
 import { GraphMenuComponent } from './content/graph-menu/graph-menu.component';
 import { IonicComponent } from './content/ionic/ionic.component';
+import { NotificationComponent } from './content/notification/notification.component';
 
 export const routes: Routes = [
 	{
@@ -9,17 +10,20 @@ export const routes: Routes = [
 		component: HelloComponent,
 	},
 	{
-		path:"graph-menu",
-		component:GraphMenuComponent,
+		path: "graph-menu",
+		component: GraphMenuComponent,
 	},
 	{
-		path:"ionic",
-		component:IonicComponent,
+		path: "ionic",
+		component: IonicComponent,
 	},
+	{
+		path:"notification",
+		component:NotificationComponent,
+	},
+	
 
-
-
-    {
+	{
 		path: "**",
 		component: HelloComponent,
 	}
